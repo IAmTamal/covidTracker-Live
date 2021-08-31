b56a618970bb1ad50d85e4f781ddd2f648dd1612
Tamal Das <gyansujan69@gmail.com>
2021-09-01 00:18:21 +0530

    Updated fonts

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <>
             <div className="container-fluid about">
                 <h3>About this project</h3>
-                <p className="ab-p">This is a basic COVID-19 TRACKER application for my State that i have made during the process of learning and praticing REACT js. <br /> This helped me understand the working of APIs, Hooks, components and objects much better </p>
+                <p className="ab-p">This is a basic COVID-19 TRACKER application for my State that i have made during the process of learning and praticing REACT js. <br /> This helped me understand the working of APIs, Hooks, components and objects much better. Since the API is realtime, it updates on a daily basis. </p>
             </div>
 
             <footer>Made with ❤ by Tamal.</footer>
