f0fcde53ba5e9c11af0b3ec52feac344ce43e98b
Tamal Das <gyansujan69@gmail.com>
2021-08-31 22:06:09 +0530

    Added API

--- src/App.js
+++ src/App.js
@@ -2,20 +2,6 @@ import React, { useEffect } from "react";
 import Covid from "./Components/Covid";
 
 const App = () => {
-  const getcovidData = async () => {
-    try {
-      const res = await fetch("https://data.covid19india.org/data.json");
-      const actualData = await res.json();
-      console.log(actualData.statewise[0]);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    //getcovidData();
-  }, []);
-
   return (
     <div>
       <Covid />
--- src/Components/Covid.js
+++ src/Components/Covid.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Covid.css";
 
 const Covid = () => {
+
+    const [data, setdata] = useState([]);
+
+    const getcovidData = async () => {
+        try {
+            const res = await fetch("https://data.covid19india.org/v4/min/data.min.json");
+            const actualData = await res.json();
+            console.log(actualData.WB.total);
+            setdata(actualData.WB.total)
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getcovidData();
+    }, []);
+
     return (
+
+
         <div className="container-fluid">
 
             <div className="headings">
@@ -15,54 +35,54 @@ const Covid = () => {
                 <div class="card c1">
                     <div className="box ">
                         <p className="card__name">
-                            <span>OUR</span> COUNTRY
+                            <span>OUR</span> STATE
                         </p>
-                        <p className="card__total card__small">INDIA</p>
+                        <p className="card__total card__small">WEST BENGAL</p>
                     </div>
                 </div>
 
                 <div class="card c1">
                     <div className="box ">
                         <p className="card__name">
-                            <span>OUR</span> COUNTRY
+                            <span>TOTAL</span> TESTED
                         </p>
-                        <p className="card__total card__small">INDIA</p>
+                        <p className="card__total card__small">{data.tested}</p>
                     </div>
                 </div>
 
                 <div class="card c1">
                     <div className="box ">
                         <p className="card__name">
-                            <span>OUR</span> COUNTRY
+                            <span>TOTAL</span> CONFIRMED
                         </p>
-                        <p className="card__total card__small">INDIA</p>
+                        <p className="card__total card__small">{data.confirmed} </p>
                     </div>
                 </div>
 
                 <div class="card c1">
                     <div className="box ">
                         <p className="card__name">
-                            <span>OUR</span> COUNTRY
+                            <span>TOTAL</span> RECOVERED
                         </p>
-                        <p className="card__total card__small">INDIA</p>
+                        <p className="card__total card__small">{data.recovered}</p>
                     </div>
                 </div>
 
                 <div class="card c1">
                     <div className="box ">
                         <p className="card__name">
-                            <span>OUR</span> COUNTRY
+                            <span>TOTAL</span> DEATH
                         </p>
-                        <p className="card__total card__small">INDIA</p>
+                        <p className="card__total card__small">{data.deceased} </p>
                     </div>
                 </div>
 
                 <div class="card c1">
                     <div className="box ">
                         <p className="card__name">
-                            <span>OUR</span> COUNTRY
+                            <span>TOTAL</span> VACCINATED
                         </p>
-                        <p className="card__total card__small">INDIA</p>
+                        <p className="card__total card__small">{data.vaccinated2}</p>
                     </div>
                 </div>
 
